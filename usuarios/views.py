6d5ef7625e1d1dd41b7494f35d7b2413733b4659
MisaelCartes <mis.cartes@duocuc.cl>
2024-10-04 01:53:32 -0300

    SE AGREGA ENDPOINT LOGIN DEVUELVE TOKENS REFRESH ACCESS

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
+
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
+from django.contrib.auth import authenticate
+from rest_framework_simplejwt.tokens import RefreshToken
+
 from .models import User
 from .serializers import UserSerializer
 from viviendas.models import Housing
@@ -53,3 +57,21 @@ def register_user(request):
             return Response({'message': 'User registered successfully'}, status=status.HTTP_201_CREATED)
         
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['POST'])
+def login_user(request):
+    # Obtiene las credenciales del cuerpo de la solicitud
+    rut = request.data.get('rut')
+    password = request.data.get('password')
+
+    # Autentica al usuario
+    user = authenticate(rut=rut, password=password)
+    if user is not None:
+        # Si la autenticación es exitosa, genera el token
+        refresh = RefreshToken.for_user(user)
+        return Response({
+            'refresh': str(refresh),
+            'access': str(refresh.access_token),
+        }, status=status.HTTP_200_OK)
+    
+    return Response({'error': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
