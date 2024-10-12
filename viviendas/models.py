af2cf3c2ca5f9ddd8f02914647f0268a6a620d08
MisaelCartes <mis.cartes@duocuc.cl>
2024-10-12 17:08:21 -0300

    creacion familias al crear usuario y endpoint para crear miembros de la familia

--- viviendas/models.py
+++ viviendas/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from usuarios.models import User
+from datetime import date
 
 class Housing(models.Model):
     address = models.CharField(max_length=255)
@@ -23,6 +24,7 @@ class FamilyMember(models.Model):
     family = models.ForeignKey(Family, on_delete=models.CASCADE)
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
+    rut = models.CharField(unique=True,null=True, blank=True) 
     relationship = models.CharField(max_length=50)
     date_of_birth = models.DateField()
     email = models.EmailField(null=True, blank=True)
@@ -30,3 +32,12 @@ class FamilyMember(models.Model):
 
     def __str__(self):
         return f"{self.first_name} {self.last_name} ({self.relationship})"
+
+    def get_age(self):
+        """Calcula la edad del miembro de la familia basada en la fecha de nacimiento."""
+        today = date.today()
+        age = today.year - self.date_of_birth.year
+        # Ajustar la edad si el cumpleaños aún no ha ocurrido este año
+        if (today.month, today.day) < (self.date_of_birth.month, self.date_of_birth.day):
+            age -= 1
+        return age
