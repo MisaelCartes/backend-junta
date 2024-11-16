ce3f2b4bef6059fcc594d8d7c38f59da27b1bfb0
Jimmy Huste Exantus <ji.exantus@duocuc.cl>
2024-11-16 11:11:09 -0300

    cambio de parametros

--- juntavecinal/settings.py
+++ juntavecinal/settings.py
@@ -87,7 +87,7 @@ DATABASES = {
     'default': dj_database_url.config(
         default=os.getenv('DATABASE_URL'),
         conn_max_age=600,
-        conn_health_checks=True,
+        ssl_require=True  # Añade esto para conexiones seguras
     )
 }
 
