99035639727fdfc42d2bc1f136cdc0d6c4847b18
MisaelCartes <mis.cartes@duocuc.cl>
2024-11-20 22:43:26 -0300

    ccorreciones endpoint se agrega comuna region modificacion certificado

--- viviendas/models.py
+++ viviendas/models.py
@@ -29,7 +29,8 @@ class FamilyMember(models.Model):
     date_of_birth = models.DateField()
     email = models.EmailField(null=True, blank=True)
     phone_number = models.CharField(max_length=15, null=True, blank=True)
-
+    comuna = models.CharField(max_length=255, blank=True, null=True)
+    region = models.CharField(max_length=255, blank=True, null=True)
     def __str__(self):
         return f"{self.first_name} {self.last_name} ({self.relationship})"
 
