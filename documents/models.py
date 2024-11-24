d442ab463ea41f57d79d079447e417e427439768
MisaelCartes <mis.cartes@duocuc.cl>
2024-11-24 18:12:42 -0300

    recepcion guardado de datos tipo de certificado y motivo

--- documents/models.py
+++ documents/models.py
@@ -4,18 +4,42 @@ from viviendas.models import FamilyMember
 # Create your models here.
 
 class CertificateRequest(models.Model):
+
+    LEGALES = 1
+    EDUCATIVOS = 2
+    LABORALES = 3
+    BANCARIOS = 4
+    MEDICOS = 5
+    GENERALES = 6
+    REASON_REQUEST = [
+        (LEGALES, 'Legales'),
+        (EDUCATIVOS, 'Educativos'),
+        (LABORALES, 'Laborales'),
+        (BANCARIOS, 'Bancarios'),
+        (MEDICOS, 'Médicos'),
+        (GENERALES, 'que se estimen convenientes'),
+    ]
+    REASONS = [LEGALES,EDUCATIVOS,LABORALES,BANCARIOS,MEDICOS,GENERALES]
+
     STATUS_CHOICES = [
         ('requested', 'Solicitado'),
         ('approved', 'Aprobado'),
         ('rejected', 'Rechazado'),
     ]
 
+    RESIDENCIA = 1
+    TYPES_CERTIFICATES = [
+        (RESIDENCIA, 'Certificado de residencia'),
+    ]
+    TYPES = [RESIDENCIA]
     # Claves foráneas
     user = models.ForeignKey(User, null=True, blank=True, on_delete=models.SET_NULL)  # Solicitante (puede ser nulo)
     family_member = models.ForeignKey(FamilyMember, null=True, blank=True, on_delete=models.SET_NULL)  # Miembro de la familia (puede ser nulo)
 
     # Otros campos
     status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='requested')  # Estado de la solicitud
+    reason_of_request = models.IntegerField(choices=REASON_REQUEST, default=GENERALES)
+    type_of_certificate = models.IntegerField(choices=TYPES_CERTIFICATES, default=RESIDENCIA)
     creation_date= models.DateTimeField(auto_now_add=True)  # Fecha de creación de la solicitud
     rejection_reason = models.TextField(blank=True, null=True)  # Justificación en caso de rechazo
     certificate_file = models.FileField(blank=True, null=True)  # Archivo del certificado
